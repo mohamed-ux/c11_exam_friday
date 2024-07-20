c1f130a180c618943e0d2811fba919c031766126
mohamed <mh187457@gmail.com>
2024-07-20 18:47:43 +0300

    edited Screens

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -1,15 +1,28 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName='home';
+
+   HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex=0;
   List<String> categories=[
     'Art','Business','Comedy','Drama','Action'
   ];
+
   List<String> images=[
    'assets/images/photo3.png',
    'assets/images/photo2.png',
+    'assets/images/photo3.png',
+    'assets/images/photo2.png',
   ];
-   HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -115,15 +128,41 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
             Container(
-              height: 400,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: images.length,
-                  itemBuilder: (context,index){
-                    return Container(
-                        margin: EdgeInsets.all(10),
-                        child: Image.asset(images[index],fit:BoxFit.fill));
-                  }),
+
+              //color: Colors.red,
+              height: 380,
+              child: CarouselSlider.builder(itemCount: images.length,
+                  itemBuilder: (BuildContext,itemIndex,pageViewIndex)=>
+                  Image.asset(images[itemIndex],fit: BoxFit.fill,height: 350,width:double.infinity,),
+                  options: CarouselOptions(
+                    height: 350,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.3,
+                    onPageChanged: (index, _) {
+                      setState(() {
+                        currentIndex = index;
+                      });
+                    },
+                    scrollDirection: Axis.horizontal,
+                  ))
+            ),
+            Center(
+              child: AnimatedSmoothIndicator(activeIndex: currentIndex,
+                  count: images.length,
+              effect: JumpingDotEffect(
+                activeDotColor: Colors.purple,
+                //offset: 5,
+                dotWidth: 8
+              )),
             ),
             SizedBox(height: 20,),
             Row(
